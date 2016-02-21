92615ab0df4c1e3d51046d74a44ce5a44f7acb18
utilisateur <utilisateur@DESKTOP-7COEMFD>
2016-02-21 13:52:58 +0100

    Adding contents :
    The Scanner utility,
    Rename "Personnage" references variable,
    A Do While structure which allow player to fight until his or cpu life
    down,
    Two lines which allow user to see who's dead.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,31 @@
+import java.util.Scanner;
+
 public class Main
 {
 	public static void main(String[] args)
 	{
-		Personnage a = new Personnage();
-		Personnage b = new Personnage();
+		Scanner saisieUtilisateur = new Scanner(System.in);
+		Personnage joueur = new Personnage();
+		Personnage cpu = new Personnage();
 		
-		a.attaquerN(b);
-		System.out.println(b.getLife());
+		do
+		{
+			System.out.println("Vous : "+joueur.getLife()+"\tCpu : "+cpu.getLife()+"\n");
+			// Player turn
+			if(joueur.getLife() > Personnage.HALF_LIFE)
+				PhasesJoueur.moitieSupérieure(joueur, cpu, saisieUtilisateur);
+			else
+				PhasesJoueur.moitieInferieure(joueur, cpu, saisieUtilisateur);
+			
+			// Cpu turn
+			if(cpu.getLife() > Personnage.HALF_LIFE)
+				PhasesCPU.moitieSuperieure(cpu, joueur);
+			else
+				PhasesCPU.moitieInferieure(cpu, joueur);
+		}while(!joueur.isDead() && !cpu.isDead());
+		saisieUtilisateur.close();
+		
+		System.out.println("joueur est mort : "+joueur.isDead());
+		System.out.println("cpu est mort : "+cpu.isDead());
 	}
 }
