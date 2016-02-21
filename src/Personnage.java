e94da15eded13507adc7352a2e1f3a9506d0b97f
utilisateur <utilisateur@DESKTOP-7COEMFD>
2016-02-21 13:46:27 +0100

    Adding content :
    The attribute mana,
    The DEFAULT_MANA constant,
    The HALF_LIFE constant which represent the half life of a "Personnage",
    The getter getMana(),
    The boolean operation isDead(),
    The boolean operation outOfMana()

--- src/Personnage.java
+++ src/Personnage.java
@@ -1,17 +1,20 @@
-
 public class Personnage
 {
+	public static final double DEFAULT_MANA = 5.0;
 	public static final double DEFAULT_LIFE = 50.0;
+	public static final double HALF_LIFE = 25.0;
 	public static final double LIFE_LIMIT = 55.0;
 	
 	private double life;
 	private double attaque;
 	private double defense;
 	private double coupDeGrace;
+	private double mana;
 	
 	public Personnage()
 	{
 		this.life = DEFAULT_LIFE;
+		this.mana = DEFAULT_MANA;
 	}
 
 	public double getLife()
@@ -33,6 +36,21 @@ public class Personnage
 	{
 		return this.coupDeGrace;
 	}
+	
+	public double getMana()
+	{
+		return this.mana;
+	}
+
+	public boolean isDead()
+	{
+		return this.life == 0;
+	}
+	
+	public boolean outOfMana()
+	{
+		return this.getMana() == 0;
+	}
 
 	public void attaquerN(Personnage cible)
 	{
@@ -40,9 +58,15 @@ public class Personnage
 			return;
 		
 		this.attaque = Math.floor(Math.random()*10 + 1);
+		System.out.println("Attaque : "+this.attaque);
 		cible.defense = Math.floor(Math.random()*9 + 2);
+		System.out.println("Défense cible : "+cible.defense+"\n");
 		
-		cible.life -= Math.abs(this.attaque - cible.defense);
+		double perte = this.attaque - cible.defense;
+		if(perte < 0)
+			perte = 0;
+		cible.life -= perte;
+		
 		if(cible.life <= 0)
 			cible.life = 0;
 	}
@@ -53,19 +77,33 @@ public class Personnage
 			return;
 		
 		this.coupDeGrace = Math.floor(Math.random()*11 + 9);
+		System.out.println("Attaque : "+this.coupDeGrace);
 		cible.defense = Math.floor(Math.random()*9 + 2);
+		System.out.println("Défense cible : "+cible.defense+"\n");
 		
-		cible.life -= Math.abs(this.coupDeGrace - cible.defense);
+		double perte = this.coupDeGrace - cible.defense;
+		if(perte < 0)
+			perte = 0;
+		cible.life -= perte;
+		
 		if(cible.life < 0)
 			cible.life = 0;
 	}
 	
 	public void seSoigner()
 	{
-		if(this.life == 0)
+		if(this.life == 0 || this.mana == 0)
 			return;
 		
-		this.life += Math.floor(Math.random()*10 + 10);
+		this.mana -= 1;
+		if(this.mana < 0)
+			this.mana = 0;
+		
+		double soins;
+		soins = Math.floor(Math.random()*10 + 10);
+		System.out.println("Soins : "+soins+"\n");
+		
+		this.life += soins;
 		
 		if(this.life > LIFE_LIMIT)
 			this.life = LIFE_LIMIT;
