5992977f2ca22b284daf63b3adae06a721772d68
Nico <Nico@DESKTOP-7COEMFD>
2016-02-22 15:54:52 +0100

    Adding contents :
    The structure which allow player to replay a game,
    The player's mana is initialized to DEFAULT_MANA only when player starts
    the application and initialized to joueur.getMana() if the player wants
    to replay.

--- src/Main.java
+++ src/Main.java
@@ -4,28 +4,48 @@ public class Main
 {
 	public static void main(String[] args)
 	{
+		boolean rejouer = false;
 		Scanner saisieUtilisateur = new Scanner(System.in);
-		Personnage joueur = new Personnage();
-		Personnage cpu = new Personnage();
-		
+		Personnage joueur = new Personnage(Personnage.DEFAULT_MANA);
+		Personnage cpu = new Personnage(Personnage.DEFAULT_MANA);
 		do
 		{
-			System.out.println("Vous : "+joueur.getLife()+"\tCpu : "+cpu.getLife()+"\n");
-			// Player turn
-			if(joueur.getLife() > Personnage.HALF_LIFE)
-				PhasesJoueur.moitieSupérieure(joueur, cpu, saisieUtilisateur);
-			else
-				PhasesJoueur.moitieInferieure(joueur, cpu, saisieUtilisateur);
+			do
+			{
+				System.out.println("Vous : "+joueur.getLife()+"\tCpu : "+cpu.getLife()+"\n");
+				// Player turn
+				if(joueur.getLife() > Personnage.HALF_LIFE)
+					PhasesJoueur.moitieSupérieure(joueur, cpu, saisieUtilisateur);
+				else
+					PhasesJoueur.moitieInferieure(joueur, cpu, saisieUtilisateur);
+
+				// Cpu turn
+				if(cpu.getLife() > Personnage.HALF_LIFE)
+					PhasesCPU.moitieSuperieure(cpu, joueur);
+				else
+					PhasesCPU.moitieInferieure(cpu, joueur);
+			}while(!joueur.isDead() && !cpu.isDead());
+
+			System.out.println("joueur est mort : "+joueur.isDead());
+			System.out.println("cpu est mort : "+cpu.isDead());
 			
-			// Cpu turn
-			if(cpu.getLife() > Personnage.HALF_LIFE)
-				PhasesCPU.moitieSuperieure(cpu, joueur);
-			else
-				PhasesCPU.moitieInferieure(cpu, joueur);
-		}while(!joueur.isDead() && !cpu.isDead());
+			// Replay ?
+			System.out.println("\nVoulez-vous rejouer ?");
+			System.out.println("1.Oui\t2.Non");
+			int replayChoice = saisieUtilisateur.nextInt();
+			
+			switch(replayChoice)
+			{
+				case 1:
+					rejouer = true;
+					joueur = new Personnage(joueur.getMana());
+					cpu = new Personnage(joueur.getMana());
+					break;
+				default:
+					rejouer = false;
+					break;
+			}
+		}while(rejouer);
 		saisieUtilisateur.close();
-		
-		System.out.println("joueur est mort : "+joueur.isDead());
-		System.out.println("cpu est mort : "+cpu.isDead());
 	}
 }
